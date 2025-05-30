0dfdad977dfb1d8e5e20397ea9c6478a0555454f
emilycostas <emilyenger@msn.com>
2025-05-30 13:22:16 +0200

    fjernet alt annet enn pdf'er og noe småting

--- DecisionTree/bcat/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import progressLog from './progressLog'
-import completedLog from './completedLog'
-
-export const schemaTypes: SchemaTypeDefinition[] = [
-  progressLog,
-  completedLog,
-]
